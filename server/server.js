f0e8fea30e9dfb89f58f04e8939fa10785287338
Vaibhav <vs630916@gmail.com>
2023-04-12 08:18:06 +0530

    Setting up Mongoose

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const workoutRouters = require('./routes/workout')
+const workoutRouters = require("./routes/workout");
 
 // express app
 const app = express();
 
 //middleware
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/workout', workoutRouters)
+app.use("/api/workout", workoutRouters);
 
 //listen for requests
 app.listen(process.env.PORT, () => {
   console.log("Hello port", process.env.PORT);
 });
- 
